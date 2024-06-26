8b9cb2a9fd620239b8f8578c02d2b16d6d7dd909
Elena Mitrofanova <tang.aloyna@gmail.com>
2024-06-26 14:47:52 +0800

    icons size and colour changed

--- src/MyMeal.js
+++ src/MyMeal.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { CiEdit } from "react-icons/ci";
+import { FaRegEdit } from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
+
 
 const MyMeal = ({text,updatingInput,deleteMeal}) => {
   return (
     <div>
         <p>{text}</p>
-        <CiEdit onClick={updatingInput} />
-        <MdDeleteOutline onClick={deleteMeal}/>
         
-
+        <FaRegEdit  onClick={updatingInput}  size={30} color={'#FFFFFF'}/>
+      
+        
+        <MdDeleteOutline  onClick={deleteMeal}  size={30} color={'#FF0000'}/>
+      
+    
     </div>
   )
 }
